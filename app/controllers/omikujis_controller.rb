94b4a14f60a8eeb085b07a4a4f55c5c91b833e0c
kzkio114 <kzkio114@gmail.com>
2025-02-13 23:56:08 +0900

    ホームで10件リアルタイム

--- app/controllers/omikujis_controller.rb
+++ app/controllers/omikujis_controller.rb
@@ -1,5 +1,5 @@
 class OmikujisController < ApplicationController
-  before_action :set_saved_histories, only: %i[create]
+  before_action :set_saved_histories, only: %i[create reset]
 
   def create
     @snake = Snake.random_snake
@@ -7,6 +7,8 @@ class OmikujisController < ApplicationController
     OmikujiHistory.create!(snake: @snake)
 
     OmikujiHistory.order(created_at: :asc).limit(OmikujiHistory.count - 10).destroy_all if OmikujiHistory.count > 10
+
+    @saved_histories = OmikujiHistory.recent
 
     respond_to do |format|
       format.turbo_stream
@@ -17,12 +19,17 @@ class OmikujisController < ApplicationController
   def reset
     session.delete(:snake_id)
     OmikujiHistory.delete_all
-    redirect_to root_path, notice: "履歴をリセットしました！"
+    @saved_histories = []
+    
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to root_path, notice: "履歴をリセットしました！" }
+    end
   end
 
   private
 
   def set_saved_histories
-    @saved_histories = OmikujiHistory.order(created_at: :desc).limit(3)
+    @saved_histories = OmikujiHistory.recent
   end
 end
--- app/models/omikuji_history.rb
+++ app/models/omikuji_history.rb
@@ -1,7 +1,10 @@
 class OmikujiHistory < ApplicationRecord
   belongs_to :snake
 
-  scope :saved, -> { where(saved: true).order(created_at: :desc).limit(10) }
+  scope :recent, -> { order(created_at: :desc).limit(10) }
 
-  broadcasts_to ->(history) { "history_list" }, inserts_by: :append, target: "history-list"
+  broadcasts_to ->(history) { "history-list" },
+                inserts_by: :append,
+                target: "history-list",
+                partial: "omikujis/history"
 end
