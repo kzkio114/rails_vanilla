636d130afeb2c16d9295426a16170ea09d976e52
kzkio114 <kzkio114@gmail.com>
2025-02-16 00:05:17 +0900

    自分履歴を5件保存

--- app/controllers/omikujis_controller.rb
+++ app/controllers/omikujis_controller.rb
@@ -8,7 +8,13 @@ class OmikujisController < ApplicationController
 
     OmikujiHistory.order(created_at: :asc).limit(OmikujiHistory.count - 10).destroy_all if OmikujiHistory.count > 10
 
+    MyHistory.create!(snake: @snake)
+    if MyHistory.count > 5
+      MyHistory.order(created_at: :asc).limit(1).destroy_all
+    end
+
     @saved_histories = OmikujiHistory.recent
+    @my_histories = MyHistory.recent
 
     respond_to do |format|
       format.turbo_stream
@@ -19,8 +25,10 @@ class OmikujisController < ApplicationController
   def reset
     session.delete(:snake_id)
     OmikujiHistory.delete_all
+    MyHistory.delete_all
     @saved_histories = []
-    
+    @my_histories = []
+
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_to root_path, notice: "履歴をリセットしました！" }
@@ -31,5 +39,6 @@ class OmikujisController < ApplicationController
 
   def set_saved_histories
     @saved_histories = OmikujiHistory.recent
+    @my_histories = MyHistory.recent
   end
 end
