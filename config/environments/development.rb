87f2fe1e58786f12df7d93a37624104f0c008b17
kzkio114 <kzkio114@gmail.com>
2025-03-06 23:03:02 +0900

    しゅうせい

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << ENV.fetch("RAILS_ALLOWED_HOST", "omikuji.fly.dev")
+  config.hosts << ENV.fetch("CLOUD_RUN_HOST", "0.0.0.0")
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
