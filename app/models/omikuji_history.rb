81e8dff6840312a28f40a393af4a6c7b07401923
kzkio114 <kzkio114@gmail.com>
2025-04-06 21:32:20 +0900

    ウェブソケットが正しく追加？

--- app/models/omikuji_history.rb
+++ app/models/omikuji_history.rb
@@ -3,7 +3,7 @@ class OmikujiHistory < ApplicationRecord
 
   scope :recent, -> { order(created_at: :desc).limit(10) }
 
-  broadcasts_to ->(history) { "public-history" },
+  broadcasts_to ->(_) { "public-history" },
     inserts_by: :prepend,
     target: "public-history-list",
     partial: "omikujis/history_item"
