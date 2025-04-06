c7cd3ae76f211fdee9fa8791b3e0c48dd67962e3
kzkio114 <kzkio114@gmail.com>
2025-04-06 19:42:26 +0900

    ウェブソケットが正しく追加

--- app/controllers/omikujis_controller.rb
+++ app/controllers/omikujis_controller.rb
@@ -1,20 +1,29 @@
 class OmikujisController < ApplicationController
   before_action :set_saved_histories, only: %i[index create reset]
 
-  def index;end
+  def index
+  end
 
   def create
     @snake = Snake.random_snake
     session[:snake_id] = @snake.id
+
     @history = OmikujiHistory.create!(snake: @snake)
 
-    OmikujiHistory.order(created_at: :asc).limit(OmikujiHistory.count - 10).destroy_all if OmikujiHistory.count > 10
-
-    MyHistory.create!(snake: @snake)
-    if MyHistory.count > 5
-      MyHistory.order(created_at: :asc).limit(1).destroy_all
+    # OmikujiHistory を10件に保つ
+    history_count = OmikujiHistory.count
+    if history_count > 10
+      OmikujiHistory.order(created_at: :asc).limit(history_count - 10).destroy_all
     end
 
+    # MyHistoryを更新
+    MyHistory.create!(snake: @snake)
+    my_history_count = MyHistory.count
+    if my_history_count > 5
+      MyHistory.order(created_at: :asc).limit(my_history_count - 5).destroy_all
+    end
+
+    # 最新の履歴を取得（スコープがモデルにある前提）
     @saved_histories = OmikujiHistory.recent
     @my_histories = MyHistory.recent
 
@@ -26,8 +35,12 @@ class OmikujisController < ApplicationController
 
   def reset
     session.delete(:snake_id)
+
+    # 履歴を全削除
     OmikujiHistory.delete_all
     MyHistory.delete_all
+
+    # 空配列にしておく（viewでループ処理してる前提）
     @saved_histories = []
     @my_histories = []
 
