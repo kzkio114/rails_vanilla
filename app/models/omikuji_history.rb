79c041f065b6056b93033b3537270fe357822f61
kzkio114 <kzkio114@gmail.com>
2025-04-13 10:34:15 +0900

    追記

--- app/models/omikuji_history.rb
+++ app/models/omikuji_history.rb
@@ -3,8 +3,14 @@ class OmikujiHistory < ApplicationRecord
 
   scope :recent, -> { order(created_at: :desc).limit(100000) }
 
-  broadcasts_to ->(history) { "public-history" },
-    inserts_by: :append,
-    target: "public-history",
-    partial: "omikujis/history_item"
+
+  broadcasts_to ->(_) { "public-history" },
+  inserts_by: :prepend,
+  target: "public-history-list",
+  partial: "omikujis/history_item"
+
+  broadcasts_to ->(_) { "my-history" },
+  inserts_by: :prepend,
+  target: "my-history-list",
+  partial: "omikujis/history_item"
 end
