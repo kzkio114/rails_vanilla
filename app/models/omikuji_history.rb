3bd35f704a42e3ffb32fa2e2fac0382670115f6a
kzkio114 <kzkio114@gmail.com>
2025-04-13 16:15:26 +0900

    追記

--- app/models/omikuji_history.rb
+++ app/models/omikuji_history.rb
@@ -1,16 +1,11 @@
 class OmikujiHistory < ApplicationRecord
   belongs_to :snake
 
-  scope :recent, -> { order(created_at: :desc).limit(100000) }
+  scope :recent, -> { order(created_at: :desc).limit(10) }
 
 
   broadcasts_to ->(_) { "public-history" },
   inserts_by: :prepend,
   target: "public-history-list",
-  partial: "omikujis/history_item"
-
-  broadcasts_to ->(_) { "my-history" },
-  inserts_by: :prepend,
-  target: "my-history-list",
   partial: "omikujis/history_item"
 end
