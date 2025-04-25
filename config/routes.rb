f246c4524e6881e32ef1e03ab9c8751077fd4df3
kzkio114 <kzkio114@gmail.com>
2025-04-25 23:39:23 +0900

    PWA

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-# config/routes.rb
-
 Rails.application.routes.draw do
   get "/up", to: proc { [200, { "Content-Type" => "text/plain" }, ["OK"]] }
 
@@ -12,4 +10,7 @@ Rails.application.routes.draw do
       post :reset
     end
   end
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 end
