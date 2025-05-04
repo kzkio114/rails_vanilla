50eca6183efdc28583c8ec582698e865b05d0fef
kzkio114 <kzkio114@gmail.com>
2025-05-04 16:43:10 +0900

    jobとモデル修正

--- app/models/snake.rb
+++ app/models/snake.rb
@@ -2,6 +2,7 @@ class Snake < ApplicationRecord
   belongs_to :omikuji_result
   has_many :snake_colors, dependent: :destroy
 
-  has_one_attached :image
-
+  def self.random_snake
+    includes(:omikuji_result, :snake_colors).order("RANDOM()").first
+  end
 end
