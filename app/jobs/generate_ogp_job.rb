45a5d3fc0812233e8674bf06a8f176538a60d602
kzkio114 <kzkio114@gmail.com>
2025-05-04 16:57:59 +0900

    job位置修正

--- app/jobs/generate_ogp_job.rb
+++ app/jobs/generate_ogp_job.rb
@@ -5,7 +5,7 @@ class GenerateOgpJob < ApplicationJob
     Rails.logger.info("🐍 OGP生成ジョブ開始 #{snake_id}")
 
     bun_path = "/root/.bun/bin/bun"
-    script_path = Rails.root.join("app/lib/tasks/generate_ogp_once.ts").to_s
+    script_path = Rails.root.join("app/lib/scripts/generate_ogp_once.ts").to_s
 
     success = system(bun_path, "run", script_path, snake_id.to_s)
     Rails.logger.info("✅ bun実行結果: #{success}")
