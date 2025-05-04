a3e072fb7a0030649d8ae9bccf4be99be73ea0d3
kzkio114 <kzkio114@gmail.com>
2025-05-04 16:27:23 +0900

    job修正

--- app/jobs/generate_ogp_job.rb
+++ app/jobs/generate_ogp_job.rb
@@ -2,6 +2,9 @@ class GenerateOgpJob < ApplicationJob
   queue_as :default
 
   def perform(snake_id)
-    system("bun run app/lib/tasks/generate_ogp_once.ts #{snake_id}")
+    Rails.logger.info("🐍 OGP生成ジョブ開始 #{snake_id}")
+    bun_path = "/root/.bun/bin/bun"
+    success = system(bun_path, "run", "app/lib/tasks/generate_ogp_once.ts", snake_id.to_s)
+    Rails.logger.info("✅ bun実行結果: #{success}")
   end
 end
